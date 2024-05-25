cfef687ba05176e5181aeb8dc4c07572e39bb2d4
ferrangarciac <50447529+ferrangarciac@users.noreply.github.com>
2024-05-25 13:45:31 +0200

    Add files via upload

---
+++ template/js/locale.js
@@ -0,0 +1,170 @@
+const LANG_EN={
+	COMMON: [
+		"Guide",									//00
+		"Item List",								//01
+		"Missable Item",							//02
+		"Gift from ",								//03
+		"Steal from ",								//04
+		"Dropped by ",								//05
+		"Ptas.",									//06
+		"Advice",									//07
+		"Field Item",								//08
+		"Shop",										//09
+		"Pick up in ",								//10
+		"Gifted from ",								//11
+		" in ",										//12
+		"Buy in ",									//13
+		"Steal from ",								//14
+		"Drop by ",									//15
+		"Go to section..."
+		],			
+					
+
+	ITEM: [			
+		"Apple",									//00
+		"Potion",									//01
+		"Super Potion",								//02
+		"Mega Potion",								//03
+		"Ether",									//04
+		"Wooden Sword",								//05
+		"Iron Sword",								//06
+		"Ultima Sword",								//07
+		"Wooden Bow",								//08
+		"Fire Bow",									//09
+		"Rainbow Bow",								//10
+		"Key Item 1",								//11
+		"Key Item 2",								//12
+		"Key Item 3",								//13
+		"Key Item 4",								//14
+		"Key Item 5",								//15
+	],			
+				
+	LOCATION: [			
+		"Village 1",								//00
+		"Forest 1",									//01
+		"Town 1",									//02
+		"Dungeon 1",								//03
+	],			
+				
+	ENEMY: [			
+		"Test Enemy 0",             				//00
+		"Test Enemy 1",             				//01
+		"Test Enemy 2",             				//02
+		"Test Enemy 3",             				//03
+		"Test Enemy 4",             				//04
+	],			
+				
+	NPC: [			
+		"Elder",									//00
+		"Girl",										//01
+		"Guard",									//02
+	],			
+				
+	ACHIEVEMENTNAME: [			
+		"Achievement Test 0",						//00
+		"Achievement Test 1",						//01
+		"Achievement Test 2",						//02
+	],
+	
+	ACHIEVEMENTDESCRIPTION: [
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+		ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+		laboris nisi ut aliquip ex ea commodo consequat.",														//00
+		"Start the adventure.",																					//01
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+		ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+		laboris nisi ut aliquip ex ea commodo consequat.",														//02
+	],
+}
+
+LANG_EN.ADVICES = [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+	ut labore et dolore magna aliqua.",																			//00
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+	ut labore et dolore magna aliqua.",																			//01
+];
+
+const LANG_ES={
+	COMMON: [
+		"Guía",										//00
+		"Lista de Objetos",							//01
+		"Objeto Perdible",							//02
+		"Entregado por ",							//03
+		"Robado a ",								//04
+		"Dejado caer por ",							//05
+		"Ptas.",									//06  //Money Name
+		"Consejo",									//07
+		"Objeto de Campo",							//08
+		"Tienda",									//09
+		"Recógelo en ",								//10
+		"Obsequio de ",								//11
+		" en ",										//12
+		"Comprar en ",								//13
+		"Robar a ",									//14
+		"Dejado caer por ",							//15
+		"Ir a sección...",							//16
+	],			
+				
+	ITEM: [			
+		"Manzana",									//00
+		"Poción",									//01
+		"Super Poción",								//02
+		"Mega Poción",								//03
+		"Éter",										//04
+		"Espada de Madera",							//05
+		"Espada de Hierro",							//06
+		"Espada Última",							//07
+		"Arco de Madera",							//08
+		"Arco de Fuego",							//09
+		"Arco Arcoiris",							//10
+		"Objeto Clave 1",							//11
+		"Objeto Clave 2",							//12
+		"Objeto Clave 3",							//13
+		"Objeto Clave 4",							//14
+		"Objeto Clave 5",							//15
+	],			
+				
+	LOCATION: [			
+		"Pueblo 1",									//00
+		"Bosque 1",									//01
+		"Ciudad 1",									//02
+		"Mazmorra 1",								//03
+	],			
+				
+	ENEMY: [			
+		"Test Enemy 0",             				//00
+		"Test Enemy 1",             				//01
+		"Test Enemy 2",             				//02
+		"Test Enemy 3",             				//03
+		"Test Enemy 4",             				//04
+	],			
+				
+	NPC: [			
+		"Mayor",									//00
+		"Chica",									//01
+		"Guardia",									//02
+	],			
+				
+	ACHIEVEMENTNAME: [						
+		"Logro Test 0",								//00
+		"Logro Test 1",								//01
+		"Logro Test 2",								//02
+	],
+	
+	ACHIEVEMENTDESCRIPTION: [
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+		ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+		laboris nisi ut aliquip ex ea commodo consequat.",														//00
+		"Comenzar una partida",																					//01
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+		ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+		laboris nisi ut aliquip ex ea commodo consequat.",														//02
+	],
+}
+
+LANG_ES.ADVICES = [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+	ut labore et dolore magna aliqua.",																			//00
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+	ut labore et dolore magna aliqua.",																			//01
+];
